395749210995ec9abb540d67f5557fde960523bc
Daewoon <pdw4904@gmail.com>
2023-11-14 21:55:17 +0900

    first commit

---
+++ src/main/java/com/jonsoft/dw/DwApplication.java
@@ -0,0 +1,13 @@
+package com.jonsoft.dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonsoft/dw/DwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonsoft.dw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
